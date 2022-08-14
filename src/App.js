03c4829d760b89811950669f6acdb47305941b1b
Andrew <106109927+uwupioner@users.noreply.github.com>
2022-08-14 13:15:27 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero"
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+import Category from "./components/Category"
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Hero/>
+      <HeadlineCards/>
+      <Food/>
+      <Category/>
+    </div>
+  );
+}
+
+export default App;
